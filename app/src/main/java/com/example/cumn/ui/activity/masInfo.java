9c2fd8e79842319c9afde6440f7ddf305d81d067
CDenus <trabajosdenus@gmail.com>
2022-04-30 15:38:18 +0200

    ajustar bien recycler

--- app/src/main/java/com/example/cumn/ui/activity/masInfo.java
+++ app/src/main/java/com/example/cumn/ui/activity/masInfo.java
@@ -6,13 +6,26 @@ import androidx.appcompat.view.menu.MenuView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.example.cumn.R;
+import com.example.cumn.io.actividadesApiAdapter;
+import com.example.cumn.models.Graph;
 import com.example.cumn.models.Models;
+import com.example.cumn.ui.adapter.actividadesAdapter;
 import com.google.gson.Gson;
 
-public class masInfo extends AppCompatActivity {
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class masInfo extends AppCompatActivity implements Callback<Models> {
+
+    private actividadesAdapter mAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +38,23 @@ public class masInfo extends AppCompatActivity {
         String actividad_id = intent.getStringExtra("actividad_id");
 
 
+        Call<Models> call = actividadesApiAdapter.getApiService().getDeportes(); //esto nos devuelve una llamada asincrona
+        call.enqueue(this);
+
         //textView.setText(strObj.get);
+
+    }
+
+    @Override
+    public void onResponse(Call<Models> call, Response<Models> response) {
+        List<Graph> actividades = response.body().getGraph();
+
+        Log.d("onResponse actividades", "Tamaño de nuestro arreglo => " + actividades.size());
+        mAdapter.setDataSet(actividades);
+    }
+
+    @Override
+    public void onFailure(Call<Models> call, Throwable t) {
 
     }
 }
---
+++ app/src/main/java/com/example/cumn/ui/adapter/infoAdapter.java
@@ -0,0 +1,16 @@
+package com.example.cumn.ui.adapter;
+
+import android.content.Context;
+
+import com.example.cumn.models.Graph;
+
+import java.util.List;
+
+public class infoAdapter{
+
+    private List<Graph> mDataSet;
+    private Context context;
+
+
+
+}
