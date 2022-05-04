24177abf9c6bee9127cf77ec8061209947843437
Sof396 <sofiadiaz.5122gmail.com>
2022-05-05 00:27:14 +0200

    el mapa funcionaaaaaaaaaaaaaaa

--- app/src/main/java/com/example/cumn/ui/activity/masInfo.java
+++ app/src/main/java/com/example/cumn/ui/activity/masInfo.java
@@ -4,11 +4,16 @@ package com.example.cumn.ui.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.view.menu.MenuView;
+import androidx.core.app.ActivityCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
+
 
 import com.example.cumn.R;
 import com.example.cumn.dato;
@@ -16,10 +21,13 @@ import com.example.cumn.io.actividadesApiAdapter;
 import com.example.cumn.models.Graph;
 import com.example.cumn.models.Models;
 import com.example.cumn.ui.adapter.actividadesAdapter;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.gson.Gson;
 
 import java.util.List;
@@ -32,19 +40,32 @@ public class masInfo extends AppCompatActivity implements OnMapReadyCallback{
 
     private actividadesAdapter mAdapter;
     private GoogleMap mMap;
+    MapView mapView;
+    GoogleMap map;
+    double latitud;
+    double longitud;
+    private MapView mMapView;
 
 
-    @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mas_info);
+        //View v = inflater.inflate(R.layout.activity_mas_info, container, false);
 
         TextView tituloView = findViewById(R.id.masInfoTitulo);
         TextView descripcionView = findViewById(R.id.masInfoDescripcion);
         TextView organizacionView = findViewById(R.id.masInfoOrganizacion);
 
-        MapView mapView = findViewById(R.id.mapita);
-        mapView.getMapAsync(this);
+        mMapView = (MapView) findViewById(R.id.mapita);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.getMapAsync(this);
+
+
+        //MapView mapView = findViewById(R.id.mapita);
+        //mapView.getMapAsync(this);
 
         Bundle datos = this.getIntent().getExtras();
         if (datos != null){
@@ -54,8 +75,8 @@ public class masInfo extends AppCompatActivity implements OnMapReadyCallback{
             String titulo = datos.getString("titulo");
             String descripcion = datos.getString("descripcion");
             String link = datos.getString("link");
-            Double latitud = datos.getDouble("latitud");
-            Double longitud = datos.getDouble("longitud");
+             latitud = datos.getDouble("latitud");
+             longitud = datos.getDouble("longitud");
             String organizacion = datos.getString("organizacion");
 
             tituloView.setText(titulo);
@@ -63,19 +84,60 @@ public class masInfo extends AppCompatActivity implements OnMapReadyCallback{
             organizacionView.setText(organizacion);
         }
 
-
-        SupportMapFragment mapFragment = SupportMapFragment.newInstance();
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.mapita, mapFragment)
-                .commit();
+        //mapView = (MapView) v.findViewById(R.id.mapita);
+        //mapView.onCreate(savedInstanceState);
 
 
+        //mapView.getMapAsync(this);
 
+
+
+
+
+
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
     }
 
     @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions().position(new LatLng(latitud, longitud)).title("Es aqui"));
+    }
+
+    @Override
+    protected void onPause() {
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mMapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mMapView.onSaveInstanceState(outState);
+    }
+
+    /*@Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
-    }
+        map.getUiSettings().setMyLocationButtonEnabled(false);
+        //map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(latitud, longitud)));
+    }*/
+
 }
