0fbecd08cf4f5633bc4d63059ca76408fcfb4f53
Sof396 <sofiadiaz.5122gmail.com>
2022-05-05 23:45:24 +0200

    el icono que le gusta a Cristian

--- app/src/main/java/com/example/cumn/ui/activity/MenuPrincipal.java
+++ app/src/main/java/com/example/cumn/ui/activity/MenuPrincipal.java
@@ -7,9 +7,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import com.example.cumn.R;
 
